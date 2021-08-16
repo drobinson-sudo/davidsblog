4a8e5a9a1a78d74812c167d6d332c7c54fd6e967
David Robinson <robinsonda615@gmail.com>
2021-08-16 17:03:08 -0500

    Add a function to functions.php file to enqueue most recent stylesheet.

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@
  * 
  * @package David's Blog
  */
+
+ function davidsblog_enqueue_scripts() {
+    wp_enqueue_style( 'style-sheet', get_stylesheet_uri() , [], filemtime( get_template_directory() . '/style.css'), 'all' );
+ }
+
+ add_action( 'wp_enqueue_scripts', 'davidsblog_enqueue_scripts' );
