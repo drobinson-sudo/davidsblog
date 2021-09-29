f82248af87c9425efb4219ca8c16d32d6d0061a8
David Robinson <robinsonda615@gmail.com>
2021-09-29 06:59:15 -0500

    Add single post template. Add the Loop to index page.

--- index.php
+++ index.php
@@ -8,9 +8,22 @@
  get_header();
 ?>
 
-    <div class="content">
-        Content Blog
-    </div>
+<div id="primary">  
+    <main id="main" class="site-main mt-5" role="main">
+        <?php
+            if ( have_posts() ) {
+                ?>
+                <div class="container">
+                    <?php
+                        while ( have_posts() ) : the_post();
+                            the_title();
+                            the_excerpt();
+                        endwhile;
+                    ?>
+                </div>
+                <?php
+            }
+        ?>
     
 <?php
 
---
+++ single.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Single post template file.
+ * 
+ * @package David's Blog
+ */
+
+ get_header();
+?>
+
+    <div class="content">
+        Single Post
+    </div>
+    
+<?php
+
+get_footer();
