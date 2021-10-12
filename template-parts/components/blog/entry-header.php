db44f406c9191a4d86f46b46154411793760c8b4
David Robinson <robinsonda615@gmail.com>
2021-10-12 08:01:13 -0500

    Add post date entry meta function to show date published and hide date edited for SEO purposes.

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -8,12 +8,14 @@
 
 $the_post_id = get_the_ID();
 $hide_title = get_post_meta( $the_post_id, '_hide_page_title', true );
+$heading_class = ! empty( $hide_title ) && 'yes' === $hide_title ? 'hide' : '';
+
 $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );
 
  ?>
  <header class="entry-header">
      <?php
-        //Featured Image
+        // Display Featured Image
         if ( $has_post_thumbnail ) {
             ?>
             <div class="entry-image mb-3">
@@ -31,6 +33,21 @@ $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );
                 </a>
             </div>
             <?php
+        }
+       
+        // Display Title
+        if ( is_single() || is_page() ) {
+            printf(
+                '<h1 class="page-title text-dark %1$s">%2$s</h1>',
+                esc_attr( $heading_class ),
+                wp_kses_post( get_the_title() )
+            );
+        } else {
+            printf(
+                '<h2 class="entry-title mb-3"><a class="text-dark" href="%1$s">%2$s</a></h2>',
+                esc_url( get_the_permalink() ),
+                wp_kses_post( get_the_title() )
+            );
         }
     ?>
  </header>
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for entry meta
+ * 
+ * @package David's Blog
+ */
+
+ ?>
+
+ <div class="entry-meta mb-3">
+    <?php davidsblog_posted_on(); ?>
+</div>
